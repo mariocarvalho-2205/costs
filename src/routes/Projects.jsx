0ad2858e32afd6c8375a3275099e471923fd4749
mario carvalho <mario.carvalho.devpython@gmail.com>
2024-01-09 16:11:18 -0300

    mensagem de criação ok

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,9 +1,20 @@
-const Projects = () => {
+import { useLocation } from "react-router-dom"
+import Message from "../components/layout/Message"
+
+function Projects () {
+
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
     return (
       <div>
-          <h1>Projects</h1>
+        <h1>Meus Projetos</h1>
+        {message && <Message type="success" msg={message} />}
       </div>
-    )
+    );
   }
   
   export default Projects
