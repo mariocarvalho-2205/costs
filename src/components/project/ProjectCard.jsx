3e8b8c9df8f98801a33010862113c563b6ed403b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-01-11 23:19:12 -0300

    atualizado

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -1,10 +1,22 @@
 import styles from './ProjectCard.module.css'
 
+// import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+
 function ProjectCard ({ id, name, budget, category, handleRemove }) {
     return (
-        <>
-            <p>{id}{name}{budget}{category}</p>
-        </>
+        <div className={styles.project_card}>
+            <h4>{name}{budget}{category}</h4>
+            <p>
+                <span>Orçamento:</span> R$ {budget}
+            </p>
+            <p className={styles.category_test}>
+                <span></span> R$ {category}
+            </p>
+            <div>
+                <p>Editar</p>
+                <p>Remover</p>
+            </div>
+        </div>
     )
 }
 
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,52 +1,56 @@
 import { useLocation } from "react-router-dom";
-import { useState, useEffect } from 'react';
-import Container from "../components/layout/Container"
-import LinkButton from "../components/layout/LinkButton"
-import ProjectCard from "../components/project/ProjectCard"
-import Message from "../components/layout/Message"
-import styles from './Projects.module.css'
+import { useState, useEffect } from "react";
+import Container from "../components/layout/Container";
+import LinkButton from "../components/layout/LinkButton";
+import ProjectCard from "../components/project/ProjectCard";
+import Message from "../components/layout/Message";
+import styles from "./Projects.module.css";
 
-function Projects () {
+function Projects() {
+	const [projects, setProjects] = useState([]);
 
-  const [ projects, setProjects ] = useState([])
+	const location = useLocation();
+	let message = "";
+	if (location.state) {
+		message = location.state.message;
+	}
 
-  
-  const location = useLocation()
-  let message = ''
-  if (location.state) {
-    message = location.state.message
-  }
-  
-  useEffect(() => {
-    fetch('http://localhost:5000/projects', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log('Projeto em projects', data)
-      setProjects(data)
-    })
-    .catch((err) => console.log('Erro em Projects', err))
-  }, [])
+	useEffect(() => {
+		fetch("http://localhost:5000/projects", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => resp.json())
+			.then((data) => {
+				console.log("Projeto em projects", data);
+				setProjects(data);
+			})
+			.catch((err) => console.log("Erro em Projects", err));
+	}, []);
 
-    return (
-      <div className={styles.project_container}>
-        <div className={styles.title_container}>
-          <h1>Meus Projetos</h1>
-          <LinkButton to="/newproject" text="Criar Projeto" />
-        </div>
-        {message && <Message type="success" msg={message} />}
-        <Container customClass="start">
-          {projects.length > 0 &&
-            projects.map((project) => (
-            <ProjectCard name={project.name} id={project.id} budget={project.budget} />
-            ))}
-        </Container>
-      </div>
-    );
-  }
-  
-  export default Projects
+	return (
+		<div className={styles.project_container}>
+			<div className={styles.title_container}>
+				<h1>Meus Projetos</h1>
+				<LinkButton to="/newproject" text="Criar Projeto" />
+			</div>
+			{message && <Message type="success" msg={message} />}
+			<Container customClass="start">
+				{projects.length > 0 &&
+					projects.map((project) => (
+						<ProjectCard
+							name={project.name}
+							id={project.id}
+							budget={project.budget}
+							// category={project.category}
+							key={project.id}
+						/>
+					))}
+			</Container>
+		</div>
+	);
+}
+
+export default Projects;
