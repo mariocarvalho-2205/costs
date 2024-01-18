6a52e9fc469da5dc5db7ae69bdd69e9765d284df
mario carvalho <mario.carvalho.devpython@gmail.com>
2024-01-18 09:32:06 -0300

    inlciuindo classes no projeto

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -4,20 +4,20 @@ import styles from './ProjectCard.module.css'
 
 function ProjectCard ({ id, name, budget, category, handleRemove }) {
     return (
-        <div className={styles.project_card}>
-            <h4>{name}{budget}{category}</h4>
-            <p>
-                <span>Orçamento:</span> R$ {budget}
-            </p>
-            <p className={styles.category_test}>
-                <span></span> R$ {category}
-            </p>
-            <div>
-                <p>Editar</p>
-                <p>Remover</p>
-            </div>
+      <div className={styles.project_card}>
+        <h4>{name}</h4>
+        <p>
+          <span>Orçamento: R$ </span> {budget}
+        </p>
+        <p className={styles.category_test}>
+          <span></span>Desenvolvimento
+        </p>
+        <div>
+          <p>Editar</p>
+          <p>Remover</p>
         </div>
-    )
+      </div>
+    );
 }
 
 export default ProjectCard
